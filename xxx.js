cda1f351150f240875a0d2f509cfa16d2bd769c4
varshini158 <148848626+varshini158@users.noreply.github.com>
2025-07-25 15:40:25 +0530

    Create xxx.js

---
+++ xxx.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+      <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+}
